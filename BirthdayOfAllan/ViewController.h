001652c002a13fa8e5fb5bcc28ae2a7c7bb5b372
kevimkin <kevimkin@gmail.com>
2015-07-04 14:57:24 -0700

    Initial Commit

---
+++ BirthdayOfAllan/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BirthdayOfAllan
+//
+//  Created by Kevin Kim on 7/4/15.
+//  Copyright (c) 2015 Kevin Kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BirthdayOfAllan/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BirthdayOfAllan
+//
+//  Created by Kevin Kim on 7/4/15.
+//  Copyright (c) 2015 Kevin Kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
